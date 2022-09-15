9ddd32461b81dd414e961a88dd616ca3556044ba
santidr <kerbindr@gmail.com>
2022-09-14 22:01:49 -0400

    DELETE task handler

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,6 +13,21 @@ export const TaskList = () => {
     setTaskList(data.result)
   }
 
+  const handleDelete = async (id: number): Promise<void> => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/tasks/delete/${id}`, { 
+        method: "DELETE" 
+      })
+      const data = await res.json()
+
+      console.log(data)
+      setTaskList(taskList?.filter(task => task.id !== id))
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   useEffect(() => {
     loadTasks()
   }, [])
@@ -21,34 +36,34 @@ export const TaskList = () => {
     <div>
       <h3>Task List</h3>
       {
-        taskList?.map((task: Task, i: number) => (
-          <Card key={i} style={{ marginBottom: '.7rem' }}>
-            <CardContent 
-              style={{ 
+        taskList?.map((task: Task) => (
+          <Card key={task.id} style={{ marginBottom: '.7rem' }}>
+            <CardContent
+              style={{
                 backgroundColor: '#1e272e',
                 display: 'flex',
-                justifyContent: 'space-between' 
+                justifyContent: 'space-between'
               }}
             >
               <div>
-                <Typography color="white" fontSize={ 20 }>{ task.title }</Typography>
-                <Typography color="white" fontSize={ 14 }>{ task.title }</Typography>
+                <Typography color="white" fontSize={20}>{task.title}</Typography>
+                <Typography color="white" fontSize={14}>{task.title}</Typography>
               </div>
 
               <div>
                 <Button
-                  style={{ marginRight: 10 }} 
+                  style={{ marginRight: 10 }}
                   variant="contained"
-                  color="inherit" 
-                  onClick={ () => console.log('Edit') }
+                  color="inherit"
+                  onClick={() => console.log('Edit')}
                 >
                   EDIT
                 </Button>
 
-                <Button 
+                <Button
                   variant="contained"
-                  color="warning" 
-                  onClick={ () => console.log('Delete') }
+                  color="warning"
+                  onClick={ () => handleDelete(task.id) }
                 >
                   DELETE
                 </Button>
